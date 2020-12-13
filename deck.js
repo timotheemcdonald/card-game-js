ca0fc727cd3365c252eef067d20531e0aa51171a
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-13 16:32:43 -0500

    card game tutorial complete

--- deck.js
+++ deck.js
@@ -10,19 +10,39 @@ export default class Deck {
         return this.cards.length
     }
 
+    pop() {
+        return this.cards.shift()
+    }
+
+    push(card) {
+        this.cards.push(card)
+    }
+
     shuffle() {
-        for (let i = this.numberOfCards - 1; i > 0; i--)
+        for (let i = this.numberOfCards - 1; i > 0; i--) {
         const newIndex = Math.floor(Math.random() * (i + 1))
         const oldValue = this.cards[newIndex]
         this.cards[newIndex] = this.cards[i]
         this.cards[i] = oldValue
     }
 }
+}
 
 class Card {
     constructor(suit, value){
         this.suit = suit
         this.value = value
+    }
+
+    get color() {
+        return this.suit === "♠" || this.suit === "♣" ? 'black' : 'red'
+    }
+    getHTML() {
+        const cardDiv = document.createElement('div')
+        cardDiv.innerText = this.suit
+        cardDiv.classList.add("card", this.color)
+        cardDiv.dataset.value = `${this.value} ${this.suit}`
+        return cardDiv
     }
 }
 
