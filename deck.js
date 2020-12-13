f543ff0878e59b3a020ad271df313967017fa948
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-13 15:18:15 -0500

    setting up functions to shuffle and grid style placement

--- deck.js
+++ deck.js
@@ -1,9 +1,21 @@
 const SUITS = ["♠", "♥", "♦", "♣"]
-const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "🧝", "🧝‍♀️", "🧝‍♂️"]
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-class Deck {
-    constructor(cards,){
-        this.cards = []
+export default class Deck {
+    constructor(cards = freshDeck()){
+        this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+        for (let i = this.numberOfCards - 1; i > 0; i--)
+        const newIndex = Math.floor(Math.random() * (i + 1))
+        const oldValue = this.cards[newIndex]
+        this.cards[newIndex] = this.cards[i]
+        this.cards[i] = oldValue
     }
 }
 
@@ -12,4 +24,12 @@ class Card {
         this.suit = suit
         this.value = value
     }
+}
+
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
 }
